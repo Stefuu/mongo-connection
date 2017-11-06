92372d1b60e634d4b6b49782fb3436a7062f2084
Stefano Damiano <sdamiano@uolinc.com>
2017-11-06 13:53:25 -0200

    Adicionando eventos customizados, contagem de eventos, e novos testes

---
+++ src/connection.js
@@ -0,0 +1,165 @@
+'use strict'
+
+require('dotenv').config()
+const mongodb = require('mongodb').MongoClient
+const log = require('debug')('[mongo-connection]')
+log.log = console.log.bind(console)
+const error = require('debug')('[mongo-connection]:error')
+
+/**/
+class Db {
+	
+	constructor(events, config) {
+		config = config || {}
+		config.options = config.options || {}
+
+		let dbUrl = 'mongodb://'
+		dbUrl += process.env.DATABASE_USER || config.user
+		dbUrl += ':'
+		dbUrl += process.env.DATABASE_PASS || config.pass
+		dbUrl += '@'
+		dbUrl += process.env.DATABASE_URI || config.uri
+		dbUrl += '/'
+		dbUrl += process.env.DATABASE_NAME || config.name
+		dbUrl += '?authMechanism=' + process.env.DATABASE_AUTH_MECHANISM || config.auth_mechanism
+
+		if(process.env.DATABASE_REPLICASET || config.replicaset) {
+			dbUrl += '&replicaSet=' + process.env.DATABASE_REPLICASET || config.replicaset  
+		}
+
+		this.db = undefined
+		this.isConnected = false
+		this.count = {
+			error: 0,
+			disconnected: 0,
+			reconnected: 0
+		}
+		this.dbUrl = dbUrl
+		this.config = config
+		this.events = events || {}
+	}
+
+	connect() {
+		return mongodb.connect(this.dbUrl, this.config.options)
+		.then(res => {
+			this.isConnected = true
+			this.db = res
+			this.binds()
+			return Promise.resolve(this.db)
+		})
+		.catch(err => {
+			error('Could not connect to mongo, retrying in 5s...')
+			setTimeout(() => {
+	        	this.connect()
+	      	}, 5000)
+			return Promise.reject(err)
+		})
+	}
+
+	disconnect() {
+		return this.db.close()
+		.then(() => {
+			this.isConnected = false
+			return Promise.resolve()
+		})
+		.catch(err => {
+			return Promise.reject(err)
+		})
+	}
+
+	/*************************
+  	* Default event handling *
+  	**************************/
+	binds() {
+	    this.db.on('connecting', () => {
+      		log('MongoDB connecting...')
+      		this.connecting()
+    	})
+
+	    this.db.on('open', () => {
+	    	this.isConnected = true
+	      	log('MongoDB connection is established')
+	      	this.open()
+	    })
+
+	    this.db.on('error', err => {
+	      	this.isConnected = false
+	      	this.count.error++
+	        error('MongoDB connection error: ' + JSON.stringify(err))
+	        this.error()
+	    })
+
+	    this.db.on('disconnected', () => {
+	    	this.isConnected = false
+	    	this.count.disconnected++
+	      	error('MongoDB disconnected!')
+	      	this.disconnected()
+	    })
+
+	    this.db.on('reconnected', () => {
+	    	this.isConnected = true
+	    	this.count.reconnected++
+	      	log('MongoDB reconnected!')
+	      	this.reconnected()
+	    })
+
+	    this.db.on('close', () => {
+	    	this.isConnected = false
+	    	this.count.error = 0
+	    	this.count.disconnected = 0
+	    	this.count.reconnected = 0
+	      	log('MongoDB closed!')
+	      	this.close()
+	    })
+
+	    // If the Node process ends, close the connection
+	    process.on('SIGINT', function() {
+	    	this.isConnected = false
+	      	this.db.close(function () {
+	        	log('Mongo default connection disconnected through app termination')
+	        	process.exit(0)
+	      	})
+	    })
+  	}
+
+  	/**************************************
+  	* Custom callbacks for event handling *
+  	***************************************/
+  	connecting() {
+		if(typeof this.events.connecting == 'function'){
+			this.events.connecting()
+		}
+	}
+
+	open() {
+		if(typeof this.events.open == 'function'){
+			this.events.open()
+		}
+	}
+
+	error() {
+		if(typeof this.events.error == 'function'){
+			this.events.error()
+		}
+	}
+
+	disconnected() {
+		if(typeof this.events.disconnected == 'function'){
+			this.events.disconnected()
+		}
+	}
+
+	reconnected() {
+		if(typeof this.events.reconnected == 'function'){
+			this.events.reconnected()
+		}
+	}
+
+	close() {
+		if(typeof this.events.close == 'function'){
+			this.events.close()
+		}
+	}
+}
+
+module.exports = Db
---
+++ test/connection.js
@@ -0,0 +1,112 @@
+'use strict'
+
+require('dotenv').config()
+const expect = require('chai').expect
+const DbClass = require('../src/connection.js')
+
+let eventCalled = {
+	disconnected: false,
+	connecting: false,
+	open: false,
+	error: false,
+	reconnected: false,
+	close: false,
+}
+
+const events = {
+	disconnected: function(){
+		eventCalled.disconnected = true
+	},
+	connecting: function(){
+		eventCalled.connecting = true
+	},
+	open: function(){
+		eventCalled.open = true
+	},
+	error: function(){
+		eventCalled.error = true
+	},
+	reconnected: function(){
+		eventCalled.reconnected = true
+	},
+	close: function(){
+		eventCalled.close = true
+	}
+}
+
+const mongo = new DbClass(events)
+
+describe('Connection', () => {
+	it('Should connect to mongo', () => {
+		return mongo.connect()
+		.then(db => {
+	    	expect(db).to.exist
+	    	expect(mongo.isConnected).to.equal(true)
+	    })
+	})
+	it('Should disconnect from mongo', () => {
+		return mongo.disconnect()
+		.then(() => {
+	    	expect(mongo.isConnected).to.equal(false)
+	    })
+	})
+})
+
+describe('Events', () => {
+  	beforeEach(() => {
+		return mongo.connect()
+		.then(db => {
+			expect(db).to.exist
+			expect(mongo.isConnected).to.equal(true)
+		})
+	})
+	afterEach(() => {
+		return mongo.disconnect()
+		.then(() => {
+	    	expect(mongo.isConnected).to.equal(false)
+	    })
+	})
+
+  	it('Should call "disconnected" callback on "disconnected" event and set status properly', () => {
+		expect(mongo.isConnected).to.equal(true)
+		expect(eventCalled.disconnected).to.equal(false)
+		mongo.db._events.disconnected()
+		expect(mongo.isConnected).to.equal(false)
+		expect(eventCalled.disconnected).to.equal(true)
+		expect(mongo.count.disconnected).to.be.greaterThan(0)
+  	})
+  	it('Should call "connecting" callback on "connecting" event', () => {
+		expect(eventCalled.connecting).to.equal(false)
+		mongo.db._events.connecting()
+		expect(eventCalled.connecting).to.equal(true)
+  	})
+  	it('Should call "open" callback on "open" event and set status properly', () => {
+		expect(eventCalled.open).to.equal(false)
+		mongo.db._events.open()
+		expect(eventCalled.open).to.equal(true)
+		expect(mongo.isConnected).to.equal(true)
+  	})
+  	it('Should call "error" callback on "error" event and set status properly', () => {
+		expect(mongo.isConnected).to.equal(true)
+		expect(eventCalled.error).to.equal(false)
+		mongo.db._events.error()
+		expect(eventCalled.error).to.equal(true)
+		expect(mongo.isConnected).to.equal(false)
+		expect(mongo.count.error).to.be.greaterThan(0)
+  	})
+  	it('Should call "reconnected" callback on "reconnected" event and set status properly', () => {
+		expect(eventCalled.reconnected).to.equal(false)
+		mongo.db._events.reconnected()
+		expect(eventCalled.reconnected).to.equal(true)
+		expect(mongo.isConnected).to.equal(true)
+		expect(mongo.count.reconnected).to.be.greaterThan(0)
+  	})
+  	it('Should call "close" callback on "close" event and set status properly', () => {
+		expect(mongo.isConnected).to.equal(true)
+		mongo.db._events.close()
+		expect(mongo.isConnected).to.equal(false)
+		expect(mongo.count.error).to.equal(0)
+		expect(mongo.count.reconnected).to.equal(0)
+		expect(mongo.count.disconnected).to.equal(0)
+  	})
+})
